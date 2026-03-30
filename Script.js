897abdb168f9a3666eb0f257b7d02aa91329b673
raven-hunter <ahmadesam20073@gmail.com>
2026-03-30 21:33:24 +0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,3 +1,24 @@
+const themeBtn = document.getElementById('theme-toggle');
+
+if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark-mode');
+    if (themeBtn) themeBtn.textContent = 'Light Mode';
+}
+
+if (themeBtn) {
+    themeBtn.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        
+        if (document.body.classList.contains('dark-mode')) {
+            themeBtn.textContent = 'Light Mode';
+            localStorage.setItem('theme', 'dark'); 
+        } else {
+            themeBtn.textContent = 'Dark Mode';
+            localStorage.setItem('theme', 'light'); 
+        }
+    });
+}
+
 const recipes = [
   {
     title: "Steak & Quinoa Power Bowl",
